ac7de372132da77051309f1b1d9660e21909807a
hakimxiao <alhakimabusolihin@gmail.com>
2025-08-23 21:06:16 +0700

    Handle maslah : *Saat kita menghapus record dari filament maka data pada Db akan terhapus tetapi pada disk public laravel kita file gambar masih ada. Solusinya adalah ini : 1. Menambahkan method hapus local data pada program yang imana data adalah hasil dari uploadan

--- app/Filament/Resources/SectionResource/Pages/EditSection.php
+++ app/Filament/Resources/SectionResource/Pages/EditSection.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\SectionResource\Pages;
 
 use App\Filament\Resources\SectionResource;
+use App\Models\section;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Storage;
 
 class EditSection extends EditRecord
 {
@@ -13,7 +15,11 @@ class EditSection extends EditRecord
     protected function getActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()->after(function (section $record) {
+                if($record->thumbnail) {
+                    Storage::disk('public')->delete($record->thumbnail);
+                }
+            }),
         ];
     }
 }
